8260b7efefe29cdce2f8481d6199389a496d0639
Bailey Stones <sto22018@byui.edu>
2025-11-01 19:31:00 -0600

    Update server.js to enable sessions, add route, and initialize passport and session for Passport

--- server.js
+++ server.js
@@ -8,12 +8,32 @@ import { connectToDb } from "./db/connection.js";
 import employeeRoutes from "./routes/employees.js";
 import positionRoutes from "./routes/positions.js";
 
+import session from "express-session";
+import passport from "./config/passport.js";
+import authRoutes from "./routes/auth.js";
+
 dotenv.config();
 connectToDb();
 
 const swaggerDocument = JSON.parse(fs.readFileSync("./swagger.json", "utf8"));
 const app = express();
 app.use(express.json());
+
+//Enable sessions and passport
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+//Initialize passport and session
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Routes
+app.use("/auth", authRoutes);
 
 app.use("/employees", employeeRoutes);
 app.use("/positions", positionRoutes);
